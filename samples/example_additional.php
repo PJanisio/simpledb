d9877819638358532473de3502dcb44b261ec051
p.janisio <p.janisio@724f7564-ce38-11de-b393-33af5e5426e5>
2009-11-10 20:46:55 +0000

    example files added to repo

---
+++ samples/example_additional.php
@@ -0,0 +1,46 @@
+<?php
+  /*
+SimpleDB
+by Pawel 'Pavlus' Janisio
+License: GPL v3
+Example file
+*/
+
+require_once('../mysql_driver.php'); //include class file
+
+$DB = new DB_MYSQL; //connect to database 
+
+
+//$DB->createDB('forgotten', 'latin1'); //create database 'forgotten' with latin1 default charset
+//echo '<br>';
+
+$DB->optimizeDB();  //database optimization
+
+//$DB->importDumpexec('/home/pavlus/public_html/class_database/samples/mysqldump.sql'); //import sql file REMEMBER to use global path!
+
+//Functions displaying mysql server statistics and variables
+
+echo $DB->dbVars();
+echo '<br>';
+
+//Display various statistics
+
+echo $DB->dbStatistics();
+echo '<br>';
+
+//Queries syntax display
+
+echo $DB->showSyntaxes();
+echo '<br>';
+
+//Locks tables db, event, func from WRITE access
+
+if($DB->LockTableRead('db','bans','func') == TRUE)
+echo 'Tables locked from write';
+echo '<br>';
+
+
+//unlocks all locked tables  
+
+$DB->Unlock();
+?>
---
+++ samples/example_basics.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+SimpleDB
+by Pawel 'Pavlus' Janisio
+License: GPL v3
+Example file
+*/
+
+require_once('../mysql_driver.php'); //include class file
+
+//How to connect and get results from any mysql table
+
+
+$DB = new DB_MYSQL; //connect to database
+
+$query1 = $DB->query('SELECT host FROM user'); //send query
+$query2 = $DB->query('SELECT password FROM user'); //send query
+$query3 = $DB->query('SELECT * FROM help_topic'); //send query :)
+$query4 = $DB->query('SELECT user FROM user'); //send query :)
+
+
+
+#First option -> u use only $DB->fetch(1) and $buffer will get result from LAST query
+
+
+	while($buffer = $DB->fetch(1)) //fetch result to array - basic method
+    	{ 
+	echo $buffer['user'].'<br>'; //where 'user' is a name of desired row in table
+    	}
+
+#Second option -> you can choose, from which query you want to get result $DB->fetch(1, $query2)
+
+
+	while($buffer = $DB->fetch(1, $query2)) 
+
+    	{ 
+	echo $buffer['password'].'<br>'; 
+    	}
+
+
+
+
+
+//Additional functions
+        
+echo 'Affected rows: '.$DB->affected(); //List affected records by last query
+echo '<br>'; 
+
+echo 'Number of rows from last query: '.$DB->numRows(); 
+echo '<br>';
+
+echo 'Number of rows from query3: '.$DB->numRows($query3); 
+echo '<br>';
+
+
+echo 'Queries: '.$DB->queries; //Number of queries done by script
+echo '<br>'; 
+
+
+echo $DB->showError(); //Show last mysql error
+
+
+
+
+?>
