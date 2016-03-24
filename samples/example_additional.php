2e1c35a91cb2abf504964a0ecd2be6d6ffd3078c
PJanisio <p.janisio@gmail.com>
2016-03-24 21:38:24 +0100

    php beautifier

--- samples/example_additional.php
+++ samples/example_additional.php
@@ -5,57 +5,53 @@ by Pawel 'Pavlus' Janisio
 License: GPL v3
 Example file
 */
+require_once ('../mysql_driver.php');
 
-require_once('../mysql_driver.php'); //include class file
+ // include class file
 
 $DB = new DB_MYSQL('localhost', '3306', 'root', 'password', 'mysql'); //connect to database (host, port, user, password, database)
 
-//$DB = new DB_MYSQL('localhost','3306','root','password', 'mysql', 2); //connect to database with debug mode = 2
-
-
-//$DB->createDB('forgotten', 'latin1'); //create database 'forgotten' with latin1 default charset
-//echo '<br>';
+// $DB = new DB_MYSQL('localhost','3306','root','password', 'mysql', 2); //connect to database with debug mode = 2
+// $DB->createDB('forgotten', 'latin1'); //create database 'forgotten' with latin1 default charset
+// echo '<br />';
 
 $DB->optimizeDB(TRUE); //database optimization  TRUE - to get output of optimized tables
 
-//Functions displaying mysql server statistics and variables
+// Functions displaying mysql server statistics and variables
 
 $variables = $DB->dbVars();
-
 echo $variables[0];
-echo '<br><br>';
+echo '<br /><br />';
 echo $variables[1];
-echo '<br><br>';
+echo '<br /><br />';
 echo $variables[2];
-echo '<br><br>';
+echo '<br /><br />';
 echo $variables[3];
-echo '<br><br>';
+echo '<br /><br />';
 echo $variables[4];
-echo '<br><br>';
+echo '<br /><br />';
 
-//Display various statistics
+// Display various statistics
 
 echo $DB->dbStatistics();
-echo '<br><br>';
+echo '<br /><br />';
 
-//Queries syntax display
+// Queries syntax display
 
 echo $DB->showSyntaxes();
-echo '<br><br>';
+echo '<br /><br />';
 
-//Show debug level
+// Show debug level
 
 echo 'Debug Level is now: ' . $DB->showDebugLevel();
-echo '<br><br>';
+echo '<br /><br />';
 
-//Locks tables db, event, func from WRITE access
+// Locks tables db, event, func from WRITE access
 
-if ($DB->LockTableRead('db', 'bans', 'func'))
-    echo 'Tables locked from write';
-echo '<br><br>';
+if ($DB->LockTableRead('db', 'bans', 'func')) echo 'Tables locked from write';
+echo '<br /><br />';
 
-
-//unlocks all locked tables  
+// unlocks all locked tables
 
 $DB->Unlock();
 ?>
