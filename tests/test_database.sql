4b540935e92e39ee2eb5f77d17fa62bcd99fc9e1
Pawel Janisio <p.janisio@gmail.com>
2024-06-05 17:53:33 +0200

    2.2.12
    - changed structure of test_database and changed workflow config

--- tests/test_database.sql
+++ tests/test_database.sql
@@ -1,8 +1,13 @@
-CREATE TABLE users (
+-- Check if the table users already exists
+SELECT 1 FROM users LIMIT 1;
+
+-- If the table doesn't exist, create it
+CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255),
     email VARCHAR(255)
 );
 
+-- Insert data into the table
 INSERT INTO users (username, email) VALUES ('Alice', 'alice@example.com');
 INSERT INTO users (username, email) VALUES ('Bob', 'bob@example.com');
